bb53301b32de6cee4057b42a22b2f7c497271d9b
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-09-21 19:39:21 +0530

    embedding genaration

---
+++ backend/test_embeddings.py
@@ -0,0 +1,16 @@
+from app.services.embedding_service import generate_embedding
+
+
+text="The school admission fee for Grade 1 is ₹50,000."
+
+
+try:
+    embedding=generate_embedding(text)
+
+    print("Embedding genarated successfully")
+    print(f"vectore dimensions:{ len(embedding)}")
+    print(f"First 5 Values:embedding[:5]")
+
+except Exception as e:
+     print("Embedding generation failed")
+     print("Error:", e)
